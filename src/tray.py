26d847aed288061f0b87c13da8436f44ee5e7153
Evan Pendergrass <130084942+NullPointer1331@users.noreply.github.com>
2026-06-02 09:34:17 -0700

    Added code ownership
    Co-authored-by: Cameron White <cameronwhite4121@users.noreply.github.com>

--- src/tray.py
+++ src/tray.py
@@ -1,3 +1,13 @@
 # Group 1: Tray Class
 # TODO: Implement the Tray class here
 # See README.md for the class contract and required methods
+
+#This code is owned by Evan and Cameron
+
+
+
+
+
+
+
+    
